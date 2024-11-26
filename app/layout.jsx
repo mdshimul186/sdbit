b640de241219893a4a7919ccb5cbe5c0e64a8ea1
mdshimul186 <62536223+mdshimul186@users.noreply.github.com>
2024-11-27 01:12:37 +0600

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'SDB IT - Software Driven Business',
-  description: 'SDB IT is a software driven business located in Warren, MI',
+  description: 'SDB IT is a software driven business located in Uttara, Dhaka',
 }
 
 export default function RootLayout({
